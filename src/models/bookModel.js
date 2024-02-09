d8b477c57a6fb91f3358a79b19fcb69ddcf8f5c7
Eljesa Krasniqi <ek61368@ubt-uni.net>
2024-02-09 12:51:21 +0100

    books model

---
+++ src/models/bookModel.js
@@ -0,0 +1,40 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../database/db-connection');
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Book',
+    tableName: 'books',
+    timestamps: false,
+  }
+);
+
+module.exports = Book;
