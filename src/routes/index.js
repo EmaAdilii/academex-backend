bb7760c4a75ef307276f93c80e6de3416f39e480
EmaAdilii <130588488+EmaAdilii@users.noreply.github.com>
2024-02-28 23:42:58 +0100

    added middleware to more routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -34,22 +34,22 @@ router.get('/allData', Controller.getAllData);
 router.get('/users', UserController.getAllUsers);
 router.get('/users/:id', UserController.getUserById);
 router.post('/users/create', createUserValidator(), validate, UserController.createUser);
-router.patch('/users/update/:id', updateUserValidator(), validate, UserController.updateUser);
-router.delete('/users/delete/:id', UserController.deleteUser);
+router.patch('/users/update/:id', [checkAuth, checkAdmin], updateUserValidator(), validate, UserController.updateUser);
+router.delete('/users/delete/:id', [checkAuth, checkAdmin], UserController.deleteUser);
 
 // Category routes
 router.get('/categories', CategoryController.getAllCategories);
 router.get('/categories/:id', CategoryController.getCategoryById);
-router.post('/categories/create', createCategoryValidator(), validate, CategoryController.createCategory);
-router.patch('/categories/update/:id', updateCategoryValidator(), validate, CategoryController.updateCategory);
-router.delete('/categories/delete/:id', CategoryController.deleteCategory);
+router.post('/categories/create', [checkAuth, checkAdmin], createCategoryValidator(), validate, CategoryController.createCategory);
+router.patch('/categories/update/:id', [checkAuth, checkAdmin], updateCategoryValidator(), validate, CategoryController.updateCategory);
+router.delete('/categories/delete/:id', [checkAuth, checkAdmin], CategoryController.deleteCategory);
 
 // Courses routes
 router.get('/courses', CourseController.getAllCourses);
 router.get('/courses/:id', [checkAuth], CourseController.getCourseById);
-router.post('/courses/create',  createCourseValidator(), validate, CourseController.createCourse);
-router.patch('/courses/update/:id', updateCourseValidator(), validate, CourseController.updateCourse);
-router.delete('/courses/delete/:id', CourseController.deleteCourse);
+router.post('/courses/create', [checkAuth, checkAdmin], createCourseValidator(), validate, CourseController.createCourse);
+router.patch('/courses/update/:id', [checkAuth, checkAdmin], updateCourseValidator(), validate, CourseController.updateCourse);
+router.delete('/courses/delete/:id', [checkAuth, checkAdmin], CourseController.deleteCourse);
 
 // User - Course routes
 router.get('/user-courses', UserCourseController.getAllUserCourses);
@@ -62,9 +62,9 @@ router.delete('/user-courses/:id/delete', UserCourseController.deleteUserCourse)
 // News routes
 router.get('/news', NewsController.getAllNews);
 router.get('/news/:id', NewsController.getNewsById);
-router.post('/news/create', createNewsValidator(), validate, NewsController.createNews);
-router.patch('/news/update/:id', updateNewsValidator(), validate, NewsController.updateNews);
-router.delete('/news/delete/:id', NewsController.deleteNews);
+router.post('/news/create', [checkAuth, checkAdmin], createNewsValidator(), validate, NewsController.createNews);
+router.patch('/news/update/:id', [checkAuth, checkAdmin], updateNewsValidator(), validate, NewsController.updateNews);
+router.delete('/news/delete/:id', [checkAuth, checkAdmin], NewsController.deleteNews);
 
 // Login route
 router.post('/login', UserController.loginUser);
@@ -72,17 +72,17 @@ router.post('/login', UserController.loginUser);
 // Book routes
 router.get('/books', BookController.getAllBooks);
 router.get('/books/:id', BookController.getBookById);
-router.post('/books/create', createBookValidator(), validate, BookController.createBook);
-router.patch('/books/update/:id', updateBookValidator(), validate, BookController.updateBook);
-router.delete('/books/delete/:id', BookController.deleteBook);
+router.post('/books/create', [checkAuth, checkAdmin], createBookValidator(), validate, BookController.createBook);
+router.patch('/books/update/:id', [checkAuth, checkAdmin], updateBookValidator(), validate, BookController.updateBook);
+router.delete('/books/delete/:id', [checkAuth, checkAdmin], BookController.deleteBook);
 
 
 // AboutUs routes
 router.get('/aboutUs', AboutUsController.getAllAboutUs);
 router.get('/aboutUs/:id', AboutUsController.getAboutUsById);
-router.post('/aboutUs/create', createAboutUsValidator(), validate, AboutUsController.createAboutUs);
-router.patch('/aboutUs/update/:id', updateAboutUsValidator(), validate, AboutUsController.updateAboutUs);
-router.delete('/aboutUs/delete/:id', AboutUsController.deleteAboutUs);
+router.post('/aboutUs/create', [checkAuth, checkAdmin], createAboutUsValidator(), validate, AboutUsController.createAboutUs);
+router.patch('/aboutUs/update/:id', [checkAuth, checkAdmin], updateAboutUsValidator(), validate, AboutUsController.updateAboutUs);
+router.delete('/aboutUs/delete/:id', [checkAuth, checkAdmin], AboutUsController.deleteAboutUs);
 
 // Contact Route
 // router.post('/contact', [contactFormValidator], ContactController.submitContactForm);
@@ -90,7 +90,7 @@ router.get('/contact', ContactController.getAllContacts);
 router.get('/contact/:id', ContactController.getContactById);
 router.post('/contact/create', ContactController.createContact);
 router.patch('/contact/update/:id', ContactController.updateContact);
-router.delete('/contact/delete/:id', ContactController.deleteContact);
+router.delete('/contact/delete/:id', [checkAuth, checkAdmin], ContactController.deleteContact);
 
 module.exports = router;
 
